177ba910546149d59926f665edf953ed63465bd6
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-12-05 17:59:44 +0100

    routeParameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore
+      optio modi molestias nobis aliquid laborum reprehenderit numquam ducimus
+      quasi.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
